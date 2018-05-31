4fe2681678fa554f15b65550bbd07bc76f889aea
Steve Kinney <hello@stevekinney.net>
2018-05-31 18:08:17 -0500

    Real time with an LCD

--- playground.js
+++ playground.js
@@ -5,33 +5,23 @@ var board = new five.Board({
 });
 
 const env = require('./env');
-const sendgrid = require('@sendgrid/mail');
-sendgrid.setApiKey(env.sendgrid);
+const Twitter = require('node-tweet-stream');
 
-const throttle = require('lodash/throttle');
-
-const openMessage = {
-  to: 'tessel-run@mailinator.com',
-  from: 'steves-tessel@stevekinney.net',
-  subject: 'The Door Has Been Opened',
-  text: 'Everything we feared is happening now.',
-  html: '<p>Everything we feared is happening now.</p>',
-};
+const t = new Twitter(env.twitter);
 
 board.on('ready', () => {
-  const door = new five.Switch({
-    pin: 'a2',
-    invert: true,
+  const lcd = new five.LCD({
+    pins: ['a2', 'a3', 'a4', 'a5', 'a6', 'a7'],
   });
 
-  const handleOpen = throttle(() => {
-    console.log('ALERTING EVERYONE TO THE TERRIBLE EVENT!');
-    sendgrid
-      .send(openMessage)
-      .then(() => console.log('Sent the warning email. Preparing to panic.'))
-      .catch(() => console.log('Something went terribly, terribly wrong'));
-  }, 2000);
+  t.on('tweet', tweet => {
+    const text = tweet.text;
+    const firstSixteen = text.slice(0, 16);
+    const secondSixteen = text.slice(17);
 
-  door.on('open', handleOpen);
-  door.on('close', () => console.log('close'));
+    lcd.cursor(0, 0).print(firstSixteen);
+    lcd.cursor(1, 0).print(secondSixteen);
+  });
+
+  t.track('#nodebots');
 });
