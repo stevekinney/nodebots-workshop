d4984b4ed61e323355930bcd4f24371187d42981
Steve Kinney <hello@stevekinney.net>
2018-05-31 14:39:55 -0500

    Actually handle the change

--- playground.js
+++ playground.js
@@ -4,16 +4,29 @@ var board = new five.Board({
   io: new Tessel(),
 });
 
+const throttle = require('lodash/throttle');
+const Barcli = require('Barcli');
+
+const graphs = {
+  temperature: new Barcli({ label: 'Temperature', range: [0, 120] }),
+  pressure: new Barcli({ label: 'Pressure', range: [0, 100] }),
+  relativeHumidity: new Barcli({ label: 'Relative Humidity', range: [0, 100] }),
+};
+
 board.on('ready', () => {
   const monitor = new five.Multi({
     controller: 'BME280',
   });
 
-  monitor.on('change', () => {
+  monitor.on('change', handleChange);
+
+  const handleChange = throttle(() => {
     const temperature = monitor.thermometer.fahrenheit;
     const pressure = monitor.barometer.pressure;
     const relativeHumidity = monitor.hygrometer.relativeHumidity;
 
-    console.log({ temperature, pressure, relativeHumidity });
-  });
+    graphs.temperature.update(temperature);
+    graphs.pressure.update(pressure);
+    graphs.relativeHumidity.update(relativeHumidity);
+  }, 470);
 });
